e6f65d62df86be685afd22bd360061b6b870d074
JaryMontoya <jarycarolinam125@gmail.com>
2024-06-15 01:07:30 -0500

    API RICK AND MORTY

--- rick-and-morty-app/src/app/app.routes.ts
+++ rick-and-morty-app/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { CharactersComponent } from './components/characters/characters.component';
+import { EpisodesComponent } from './components/episodes/episodes.component';
+import { LocationsComponent } from './components/locations/locations.component';
+import { MenuComponent } from './components/menu/menu.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: MenuComponent },
+  { path: 'characters', component: CharactersComponent },
+  { path: 'episodes', component: EpisodesComponent },
+  { path: 'locations', component: LocationsComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' } 
+];

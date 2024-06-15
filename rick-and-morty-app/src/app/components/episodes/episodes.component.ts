504e4baf85c15f2107d1e0c4d97ac83507aa7dd2
JaryMontoya <jarycarolinam125@gmail.com>
2024-06-15 01:22:35 -0500

    API RICK AND MORTY

--- rick-and-morty-app/src/app/components/episodes/episodes.component.ts
+++ rick-and-morty-app/src/app/components/episodes/episodes.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { RickAndMortyService } from 'src/app/services/rick-and-morty.service';
+import {RickAndMortyService} from 'src/app/services/rick-and-morty.service';
 
 @Component({
   selector: 'app-episodes',
